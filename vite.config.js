66d3ac50adfb4fa6bdc4e9d685f356aa82313aec
davm07 <diegovasquezmejia1999@gmail.com>
2024-04-06 00:46:36 +0200

    fix: correct path to team page(hopefully)

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         main: resolve(__dirname, 'src/index.html'),
         leagues: resolve(__dirname, 'src/leagues/index.html'),
         leagueDetails: resolve(__dirname, 'src/leagues/league-detail.html'),
-        team: resolve(__dirname, 'src/leagues/index.html'),
+        team: resolve(__dirname, 'src/team/index.html'),
       },
     },
   },
